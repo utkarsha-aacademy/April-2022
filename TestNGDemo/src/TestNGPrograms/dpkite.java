6bf8b5115f98c8bf90dda0f843ecc963df2e26c8
shubham-rakshe <shubhamrakshe92@gmail.com>
2022-08-06 19:50:17 +0530

    updated test data

--- TestNGDemo/src/TestNGPrograms/dpkite.java
+++ TestNGDemo/src/TestNGPrograms/dpkite.java
@@ -8,6 +8,7 @@ public class dpkite {
 	    return new Object[][] {
 	      new Object[] { "XA0634", "123454321", "122331" },
 	      new Object[] { "XA0342", "1234532" , "231111" },
+	      new Object[] { "XA0345", "12345746" , "111111" },
 	      new Object[] { "XA0634", "Sarika699!", "691991" },
 	    };
 	  }
